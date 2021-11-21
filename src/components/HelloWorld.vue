e06079beeeba0e9a3f4b292dafa4e0683985d028
bqc <6575734@qq.com>
2021-11-21 21:25:25 +0800

    feat: 🎸 ts高阶语法使用

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -14,6 +14,31 @@ const exposeHwFn = () => {
   return hwData.value;
 };
 defineExpose({ exposeHwFn });
+interface Person {
+  name: string;
+  age: string;
+  gender: number;
+}
+
+interface PersonVal {
+  name: string;
+}
+const personVal: Person = {
+  name: '123',
+  age: '123',
+  gender: 0,
+};
+type name = string;
+type isName = PersonVal extends Person ? 'string' : 'boolean';
+
+// 获取personVal的类型，并读取personVal类型的key
+type PersonNew = keyof typeof personVal;
+const valName: isName = 'boolean';
+
+type ReadOnlyType<T> = {
+  readonly [p in keyof T]: T[p];
+};
+type ReadOnlyPerson = ReadOnlyType<Person>;
 </script>
 
 <template>
