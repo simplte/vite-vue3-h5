663dca1f3903131ef25a0593759b4ca6b2f91a27
bqc <6575734@qq.com>
2022-03-02 21:29:35 +0800

    feat: 🎸 ts 分布式条件类型

--- src/utils/Learnts.ts
+++ src/utils/Learnts.ts
@@ -54,3 +54,22 @@ interface BQC {
 type B = Extract<keyof BQC, 'q' | 'b'>;
 type C = Extract<'d', 'q' | 'b'>; // never
 const bv: B = 'b';
+
+// 分布式条件类型要发生的前提条件：
+
+// 首先，你得是联合类型
+// 其次，你的联合类型需要是通过泛型参数的形式传入
+// 最后，你的泛型参数在条件类型语句中需要是裸类型参数，即没有被 [] 包裹
+
+// 分布式条件类型的应用
+
+// 提取 集合T 中也存在于 集合U 中的类型分支，即 T 与 U 的交集
+type ExtractM<T, U> = T extends U ? T : never;
+// Exclude，提取 集合T 中不存在于 集合U 中的类型分支，即 T 相对于 U 的差集 T有 U没有
+type Exclude<T, U> = T extends U ? never : T;
+// 移除集合中的 null 与 undefined
+type NonNullable<T> = T extends null | undefined ? never : T;
+// 并集
+type Concurrence<A, B> = A | B;
+// 补集： 满足  B 属于 A  得到 A中有 B没有的部分
+type Complement<A, B extends A> = Exclude<A, B>;
