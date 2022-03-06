2890180607d8f5554df474acca8b587ea990dc6c
bqc <6575734@qq.com>
2022-03-06 20:33:04 +0800

    feat: 🎸 虚拟列表实现 还存在问题

---
+++ src/components/Scroll.vue
@@ -0,0 +1,149 @@
+<template>
+  <div
+    ref="wrapper"
+    style="
+      position: relative;
+      width: 100%;
+      height: 100%;
+      padding: 0;
+      margin: 0;
+      overflow: auto;
+      border: none;
+    "
+    @scroll="refreshView()"
+  >
+    <div
+      :style="{ height: listTotalHeight + 'px' }"
+      style="width: 100%; padding: 0; margin: 0"
+    ></div>
+    <div
+      ref="itemWrapper"
+      style="position: absolute; top: 0; left: 0; width: 100%; padding: 0; margin: 0"
+    >
+      <div v-for="d in listViewWithInfo" :key="d.index" :style="{ 'min-height': d.height + 'px' }">
+        <slot :item="d.item" :height="d.height" :offset="d.offset" :index="d.index"></slot>
+      </div>
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+import { computed, onMounted, reactive, ref, watch } from 'vue';
+const props = defineProps<{
+  list: Array<any>;
+  itemHeightGetter: Function;
+  defaultItemHeight: Number;
+}>();
+const emits = defineEmits(['scroll']);
+let listView = ref<any[]>([]);
+let listTotalHeight = ref<number>(0);
+let itemOffsetCache = ref<any[]>([]);
+let topItemIndex = ref<number>(0);
+let _viewHeight: number | undefined = undefined;
+let wrapper = ref<HTMLElement>();
+let itemWrapper = ref<HTMLElement>();
+let listViewWithInfo = computed(() => {
+  return listView.value.map((item, viewIndex) => {
+    const index = topItemIndex.value + viewIndex;
+    const { height, offset } = getItemInfo(index);
+    return {
+      index,
+      height,
+      item,
+      offset,
+    };
+  });
+});
+watch(
+  () => {
+    return props.list;
+  },
+  () => {
+    refreshView();
+  }
+);
+onMounted(() => {
+  refreshView({ resize: true });
+});
+const getItemInfo = (index) => {
+  // 超出取值范围，返回默认值
+  if (index < 0 || index > props.list.length - 1) {
+    return {
+      offset: 0,
+      height: 0,
+    };
+  }
+  let cache = itemOffsetCache.value[index];
+  // 如果没有缓存，进行计算并缓存结果
+  if (!cache) {
+    // 优先用itemHeightGetter计算高度，无itemHeightGetter则取defaultItemHeight作为高度
+    let height = props.itemHeightGetter
+      ? props.itemHeightGetter(props.list[index], index)
+      : props.defaultItemHeight;
+    cache = itemOffsetCache.value[index] = {
+      height, // item高度
+      offset: getItemInfo(index - 1).offset + height, // 递归得出item的bottom距离列表顶部的距离，item的offset = 上个item的offset + 自己的height
+    };
+  }
+  // 如果已有缓存，直接返回缓存的结果
+  return cache;
+};
+const refreshView = (config?) => {
+  if (config) {
+    if (config?.resize) {
+      _viewHeight = wrapper?.value?.clientHeight;
+    }
+    if (config?.clearCache) {
+      itemOffsetCache.value = [];
+    }
+  }
+  const scrollTop = wrapper?.value?.scrollTop as number;
+  const viewHeight = _viewHeight as number;
+  const _topItemIndex = findItemIndexByOffset(scrollTop);
+  const bottomItemIndex = findItemIndexByOffset(scrollTop + viewHeight);
+  topItemIndex.value = _topItemIndex;
+  listView.value = props.list.slice(topItemIndex.value, bottomItemIndex);
+  const _listTotalHeight = props.defaultItemHeight
+    ? getItemInfo(itemOffsetCache.value.length - 1).offset +
+      (props.list.length - itemOffsetCache.value.length) * props.defaultItemHeight
+    : getItemInfo(props.list.length - 1).offset;
+  listTotalHeight.value = _listTotalHeight;
+  console.log(listTotalHeight.value);
+  itemWrapper.value.style.transform = `translateY(${getItemInfo(topItemIndex.value - 1).offset}px)`; // 控制translateY使可视列表位置保持在可视窗口 ;
+  emits('scroll', {
+    topItemIndex: topItemIndex.value,
+    bottomItemIndex,
+    listTotalHeight: listTotalHeight.value,
+    scrollTop,
+  });
+};
+const findItemIndexByOffset = (offset): number => {
+  // 如果offset大于缓存数组的最后项，按序依次往后查找（调用getItemInfo的过程也会缓存数组）
+  if (offset >= getItemInfo(itemOffsetCache.value.length - 1).offset) {
+    for (let index = itemOffsetCache.value.length; index < props.list.length; index++) {
+      if (getItemInfo(index).offset > offset) {
+        return index;
+      }
+    }
+    return props.list.length - 1;
+  } else {
+    // 如果offset小于缓存数组的最后项，那么在缓存数组中二分法查找
+    let begin = 0;
+    let end = itemOffsetCache.value.length - 1;
+    while (begin < end) {
+      let mid = ((begin + end) / 2) | 0;
+      let midOffset = getItemInfo(mid).offset;
+      if (midOffset === offset) {
+        return mid;
+      } else if (midOffset > offset) {
+        end = mid - 1;
+      } else {
+        begin = mid + 1;
+      }
+    }
+    if (getItemInfo(begin).offset < offset && getItemInfo(begin + 1).offset > offset) {
+      begin = begin + 1;
+    }
+    return begin;
+  }
+};
+</script>
--- src/views/index/index.vue
+++ src/views/index/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <van-skeleton title :row="13" :loading="loading">
+  <!-- <van-skeleton title :row="13" :loading="loading">
     <van-swipe class="my-swipe">
       <van-swipe-item>
         <van-image width="100" height="100" src="https://img.yzcdn.cn/vant/cat.jpeg" />
@@ -28,13 +28,43 @@
       <van-grid-item icon="photo-o" text="文字" />
       <van-grid-item icon="photo-o" text="文字" />
     </van-grid>
-  </van-skeleton>
+  </van-skeleton> -->
+  <div class="m-container">
+    <div class="m-header">我是头部</div>
+    <div class="m-list">
+      <div class="m-list-container">
+        <Scroll
+          ref="list-view"
+          :list="list"
+          :item-height-getter="itemHeightGetter"
+          :default-item-height="defaultItemHeight"
+          @scroll="listScroll"
+        >
+          <template #default="scope">
+            <div class="item">
+              <div v-if="scope.item.no === 1">
+                代码中是有2000条的数据的，但是我们在渲染的时候刚好满足可视窗口高度的列表，因为代码中为了显示特别一点，还特意加了一个独特的高度的，所以在滚动的时候无法精确到每次都有一定的条数。`item-height-getter`可以通过这个来添加个别`Item`特别的高度。`default-item-height`这个可以设置item默认的高度，在没有设置`item-height-getter`情况下，就可以固定每次显示多少条，因为高度都是一样的，可视窗口就那么宽。有兴趣的朋友可以自己去试一下，这里就不做展示了。
+              </div>
+              <div v-else :style="{ color: scope.item.color }">
+                NO: {{ scope.item.no }}, height: {{ scope.height }}px, offset: {{ scope.offset }}px
+              </div>
+            </div>
+          </template>
+        </Scroll>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
 import { onMounted, ref, watchEffect } from 'vue';
 
+import Scroll from '@/components/Scroll.vue';
 import { LinbuduLiteralType } from '@/utils/Learnts';
+type ListInfo = {
+  no: number;
+  color: string;
+};
 const Case = new LinbuduLiteralType();
 console.log(Case.value.endsWith('u'));
 
@@ -42,10 +72,48 @@ let loading = ref<boolean>(true);
 let active = ref<number>(0);
 onMounted(() => {
   loading.value = false;
+  getData().then((d) => {
+    list.value = d;
+  });
 });
 watchEffect(() => {
   console.log(active.value);
 });
+const list = ref<any[]>([]);
+const page = ref<number>(0);
+const itemHeightGetter = (item) => {
+  if (item.no % 33 === 0) {
+    return 27;
+  }
+  return 20 + (item.no % 10);
+};
+const defaultItemHeight = 30;
+let _getting = false;
+const listScroll = (data) => {
+  if (!_getting && data.bottomItemIndex >= list.value.length - 3) {
+    _getting = true;
+    getData().then((d) => {
+      list.value.push(...d);
+      page.value++;
+      _getting = false;
+    });
+  }
+};
+const getData = (): Promise<ListInfo[]> => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      const baseIndex = page.value * 20;
+      resolve(
+        new Array(2000).fill(0).map((i, index) => {
+          return {
+            no: baseIndex + index,
+            color: ['#33d', '#3d3', '#d33', '#333'][(Math.random() * 4) | 0],
+          };
+        })
+      );
+    }, 100);
+  });
+};
 </script>
 <style lang="less">
 .my-swipe .van-swipe-item {
