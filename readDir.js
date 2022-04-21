eb1775f11a331766638da0dbae55283a5bb9e582
bqc <6575734@qq.com>
2022-04-21 22:33:04 +0800

    feat: 🎸 fs node

--- readDir.js
+++ readDir.js
@@ -27,7 +27,15 @@ dir.then((res) => {
   console.log(replaceExt(dirList[0], '.jss'));
 });
 const root = path.resolve(__dirname, './src');
-console.log(path.join(root, 'views'));
+console.log(root);
+// console.log(path.join(root, 'views'));
+/**
+ * path.relative(from, to)
+ * from: 该文件路径将用作基本路径。
+ * to:该文件路径将用于查找相对路径
+ * 一句话总结 基于from路径去找 to路径
+ */
+console.log(path.relative(root, `G:/项目/mine/vue3-vite-移动端项目/vite-vue3-h5/server`));
 
 function replaceExt(filePath, ext = '') {
   const dirName = path.dirname(filePath);
